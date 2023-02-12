d030f51546a1258531592b4a37b336ab242bfb52
David Albert <davidalbert@MacBook-Air-som-tillhor-David.local>
2023-02-12 15:34:09 +0100

    first commit

--- Lab09/src/RaceWindow.java
+++ Lab09/src/RaceWindow.java
@@ -6,12 +6,12 @@ public class RaceWindow extends SimpleWindow {
 	public static final int X_END_POS = 700;
 	public static final int Y_LINE_START = 100;
 	public static final int Y_LINE_END = 280;
-	
+
 	public RaceWindow() {
 		super(800, 400, "Race Window");
 		setUp();
 	}
-	
+
 	private void setUp(){
 		super.moveTo(X_START_POS-10, Y_LINE_START+24);
 		super.writeText("1");
@@ -29,14 +29,14 @@ public class RaceWindow extends SimpleWindow {
 		super.writeText("7");
 		super.moveTo(X_START_POS-10, Y_LINE_START+164);
 		super.writeText("8");
-			
+
 		super.moveTo(X_START_POS, Y_LINE_START);
 		super.lineTo(X_START_POS, Y_LINE_END);
-		
+
 		super.moveTo(X_END_POS, Y_LINE_START);
 		super.lineTo(X_END_POS, Y_LINE_END);
 	}
-	
+
 	public static int getStartXPos(int startNbr){
 		return X_START_POS;
 	}
